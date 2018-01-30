bb92d5bc4014a6a3e7bbca26fe7a7dd60e83a4fb
Tlenc <tlenciauskas@gmail.com>
2018-01-30 15:12:14 +0200

    Code(sum skirt)

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,15 @@
 package com.company;
-
+import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        System.out.println("Įveskite du skaičius");
+        Scanner sc = new Scanner(System.in);
+        float psk = sc.nextFloat();
+        float ask = sc.nextFloat();
+        float skirtumas = psk - ask;
+        float suma = psk + ask;
+        System.out.println("Skirtumas :" + skirtumas);
+        System.out.println("Suma : " + suma);
     }
 }
